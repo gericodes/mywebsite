2746d9e00ae15e30973a625be0bc33dca0038bf1
Geri Georgieva <84628265+gericodes@users.noreply.github.com>
2026-05-26 18:58:48 +0200

    Add files via upload

--- listings-data.js
+++ listings-data.js
@@ -955,7 +955,7 @@ const LISTINGS = [
     location: '📍 Асеновград',
     img: 'https://github.com/gericodes/mywebsite/blob/main/49/1.jpg?raw=true',
     feats: ['🏭 Производствен цех', '📐 927 м²'],
-    href: 'listing.html?id=49'
+    href: 'listing.html?id=50'
   }
 ];
 
